1807400225d19dc77952e14bc21728cad53641d2
dinhlongviolin1 <dinhlongviolin1@gmail.com>
2024-07-23 00:48:56 +0700

    initial config

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,21 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "ast-grep",
+      "typescript-language-server",
+      "prettier",
+      "eslint-lsp",
+      "autoflake",
+      "autopep8",
+      "black",
+      "flake8",
+      "harper-ls",
+      "isort",
+      "mypy",
+      "pyright",
+      "ruff",
+      "sourcery",
+    },
+  },
+}
