71ef3ba073ceb67329ce9aebee3638abb39e5068
dinhlongviolin1 <dinhlongviolin1@gmail.com>
2025-03-31 22:45:34 +0700

    add venv selector back

---
+++ lua/plugins/venvselector.lua
@@ -0,0 +1,12 @@
+return {
+  "linux-cultist/venv-selector.nvim",
+  dependencies = {
+    "neovim/nvim-lspconfig",
+    { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+  },
+  lazy = false,
+  branch = "regexp",
+  keys = {
+    { "<leader>cv", "<cmd>VenvSelect<cr>", desc = "Select Python Virtual Env" },
+  },
+}
