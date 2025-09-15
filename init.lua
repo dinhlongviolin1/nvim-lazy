3b31b5af56e671610a92692b8fa4b53c0a69bd70
Dinh Long Nguyen <dinhlongviolin1@gmail.com>
2025-09-15 14:51:21 +0700

    setup lazygit nvr

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+local socket = "/tmp/nvim" .. vim.fn.getpid()
+vim.fn.setenv("NVIM_LISTEN_ADDRESS", socket)
---
+++ setup-lazygit.sh
@@ -0,0 +1,3 @@
+brew install neovim-remote lazygit
+mkdir ../lazygit
+cp ../lazygit-config.yml ../lazygit/config.yml
